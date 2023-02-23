23110812594d41594e76af635fed1e2d8d5eb3a1
deemacauses <deemacauses@outlook.com>
2023-02-23 22:47:17 +0200

    New: Enhance through using GetCurrentUser decorator

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -6,7 +6,6 @@ import {
   NotFoundException,
   Post,
   Body,
-  Request,
   Patch,
   Delete,
 } from "@nestjs/common";
@@ -14,7 +13,7 @@ import {
 import { PostService } from "./post.service";
 import { Post as PostModel } from "./model/post.model";
 import { PostDTO } from "./dto/post.dto";
-import { Roles } from "./../../common/decorators";
+import { GetCurrentUser, Roles } from "./../../common/decorators";
 
 @Controller("posts")
 export class PostController {
@@ -31,7 +30,7 @@ export class PostController {
   async findOne(@Param("id") id: number): Promise<PostModel> {
     // Find the post with this id
     const post = await this.postService.findOne(id);
-    // If the post doesn't exit in the db, throw a 404 error
+    // If the post doesn't exit in the database, throw a 404 error
     if (!post) {
       throw new NotFoundException("This Post doesn't exist");
     }
@@ -43,34 +42,34 @@ export class PostController {
   @Post()
   async createPost(
     @Body() post: PostDTO,
-    @Request() request,
+    @GetCurrentUser() user: { id: number },
   ): Promise<PostModel> {
     // Create a new post and return the newly created post
-    return await this.postService.createPost(post, request.user.id);
+    return await this.postService.createPost(post, user.id);
   }
 
   @Roles("user")
   @Post("/:id/comments")
   async createComment(
     @Param("id") id: number,
-    @Request() request,
+    @GetCurrentUser() user: { id: number },
     @Body() comment: CommentDTO,
   ): Promise<CommentDTO> {
-    return await this.postService.createComment(id, request.user.id, comment);
+    return await this.postService.createComment(id, user.id, comment);
   }
 
-  @Roles("user")
+  @Roles("admin")
   @Patch(":id")
   async update(
     @Param("id") id: number,
     @Body() post: PostDTO,
-    @Request() request,
+    @GetCurrentUser() user: { id: number },
   ): Promise<PostModel> {
     // Get the number of row affected and the updated post
     const { numberOfAffectedRows, updatedPost } = await this.postService.update(
       id,
       post,
-      request.user.id,
+      user.id,
     );
     // If the number of row affected is zero, it means the post doesn't exist in our database
     if (numberOfAffectedRows === 0) {
@@ -82,9 +81,12 @@ export class PostController {
 
   @Roles("admin")
   @Delete(":id")
-  async remove(@Param("id") id: number, @Request() request) {
+  async remove(
+    @Param("id") id: number,
+    @GetCurrentUser() user: { id: number },
+  ) {
     // Delete the post with this id
-    const deleted = await this.postService.delete(id, request.user.id);
+    const deleted = await this.postService.delete(id, user.id);
     // If the number of row affected is zero, then the post doesn't exist in our database
     if (deleted === 0) {
       throw new NotFoundException("This Post doesn't exist");
